e905f776c6d7309374b6fe46569aed792fb7c4bd
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2023-06-20 15:28:47 -0400

    Add files via upload

---
+++ data2.js
@@ -0,0 +1,26 @@
+const jobs = [
+    {
+      id: 1,
+      title: 'Front-end Developer',
+      description: 'We are looking for a skilled front-end developer to join our team.',
+      location: 'New York, USA',
+      department: 'Development'
+    },
+    {
+      id: 2,
+      title: 'Backend Developer',
+      description: 'We are hiring a backend developer to work on our server infrastructure.',
+      location: 'London, UK',
+      department: 'Development'
+    },
+    {
+      id: 3,
+      title: 'UX Designer',
+      description: 'Join our UX design team and help create intuitive user experiences.',
+      location: 'San Francisco, USA',
+      department: 'Design'
+    }
+    // Add more job positions as needed
+  ];
+  
+  export default jobs;
