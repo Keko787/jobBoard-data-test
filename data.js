8f9a1dfb072c6acf2eb6660434d54c9d4eb18f9b
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2023-06-21 15:40:00 -0400

    Update data.js

--- data.js
+++ data.js
@@ -8,7 +8,7 @@ let jobsdata = [  /* You can add more positions and information based on the for
     
     { "id": 1, 
       "title": "Front-end Developer", 
-      "description": "test12456",
+      "description": "hello this is a test",
       "location": "New York, USA", 
       "department": "Development" 
     },
