55cc2fdf5c444611b6a4f415a06c75ee8a763b9c
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2023-06-21 11:30:58 -0400

    Add files via upload

---
+++ data4.js
@@ -0,0 +1,23 @@
+let jobsdata = [
+    { "id": 1, 
+      "title": "Front-end Developer", 
+      "description": "test124",
+      "location": "New York, USA", 
+      "department": "Development" 
+    },
+    {
+      "id": 2,
+      "title": "Backend Developer",
+      "description": "We are hiring a backend developer to work on our server infrastructure.",
+      "location": "London, UK",
+      "department": "Development"
+    },
+    {
+      "id": 3,
+      "title": "UX Designer",
+      "description": "Join our UX design team and help create intuitive user experiences.",
+      "location": "San Francisco, USA",
+      "department": "Design"
+    }
+  ]
+  
