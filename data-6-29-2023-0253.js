95eb8ea51d6e1855db98ebbbce47006124e85110
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2023-06-29 14:53:37 -0400

    Update and rename data-6-29-2023-1110.js to data-6-29-2023-0253.js

--- data-6-29-2023-0253.js
+++ data-6-29-2023-0253.js
@@ -1,16 +1,25 @@
 let jobsdata = [  /* You can add more positions and information based on the format below */
     /* { "id": #, 
+      "title": "Position", 
+      "description": "example ",  // (you can add html elements like <strong> to make text bold and <ul> and <li> to makes lists, the only exception is <p> because it is the default in the modal window)
+      "location": "City, State ",  // (abbreiviate State)
+      "locationSlug": "city-state",  // (make sure its lowercase and a dash between city and state, otherwise you may have to use the slugify api and lowercase)
+      "department": "Department"
+    },  // the comma matters if you want to add another entry after, the last entry doesnt need a comma at the end
+
+    // you can copy this as a template
+    { "id": #, 
       "title": "position", 
-      "description": "example (you can add html elements except for <p> because it is the default)",
-      "location": "City, State (abbreiviate State)", 
-      "locationSlug": "city-state",
-      "department": "Department Name"
-      
-    }, */
+      "description": "example ",  
+      "location": "City, State ",  
+      "locationSlug": "city-state",  
+      "department": "Department"
+    },
+    */
     
     { "id": 1, 
       "title": "Front-end Developer", 
-      "description": "please work again",
+      "description": "<strong> please work </strong> again",
       "location": "Jacksonville, FL", 
       "locationSlug": "jacksonville-fl",
       "department": "Development"
@@ -19,7 +28,11 @@ let jobsdata = [  /* You can add more positions and information based on the for
     {
       "id": 2,
       "title": "Backend Developer",
-      "description": "We are hiring a backend developer to work on our server infrastructure.",
+      "description": "We are hiring a backend developer to work on our server infrastructure. <ul>
+          <li> test 1. </li>
+          <li> test 2. </li>
+          <li> test 3. </li>
+          </ul>",
       "location": "Pittsburgh, PA",
       "locationSlug": "pittsburgh-pa",
       "department": "Development"
@@ -57,4 +70,3 @@ let jobsdata = [  /* You can add more positions and information based on the for
       "department": "Drafting"
     }
   ]
-  
