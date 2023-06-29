33d78d86fc909352d1b4c9990a3467b6cd91676d
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2023-06-29 11:20:56 -0400

    Update and rename data-6-29-2023-1030.js to data-6-29-2023-1110.js

--- data-6-29-2023-1110.js
+++ data-6-29-2023-1110.js
@@ -3,34 +3,41 @@ let jobsdata = [  /* You can add more positions and information based on the for
       "title": "position", 
       "description": "example (you can add html elements except for <p> because it is the default)",
       "location": "City, State (abbreiviate State)", 
-      "department": "Department Name" 
+      "locationSlug": "city-state",
+      "department": "Department Name"
+      
     }, */
     
     { "id": 1, 
       "title": "Front-end Developer", 
       "description": "please work again",
-      "location": "New York, USA", 
-      "department": "Development" 
+      "location": "Jacksonville, FL", 
+      "locationSlug": "jacksonville-fl",
+      "department": "Development"
+      
     },
     {
       "id": 2,
       "title": "Backend Developer",
       "description": "We are hiring a backend developer to work on our server infrastructure.",
-      "location": "London, UK",
+      "location": "Pittsburgh, PA",
+      "locationSlug": "pittsburgh-pa",
       "department": "Development"
     },
     {
       "id": 3,
       "title": "UX Designer NOT LOL",
       "description": "Join our UX design team and help create intuitive user experiences.",
-      "location": "San Francisco, USA",
+      "location": "Pittsburgh, PA",
+      "locationSlug": "pittsburgh-pa",
       "department": "Design"
     },
     {
       "id": 4,
       "title": "UX Designess",
       "description": "Join our UX design team and help creas.",
-      "location": "Francisco, USA",
+      "location": "Jacksonville, FL",
+      "locationSlug": "jacksonville-fl",
       "department": "Design"
     },
     {
@@ -38,13 +45,15 @@ let jobsdata = [  /* You can add more positions and information based on the for
       "title": "Engineer",
       "description": "Join us to plan and build things.",
       "location": "Jacksonville, FL",
+      "locationSlug": "jacksonville-fl",
       "department": "Engineering"
     },
     {
       "id": 6,
       "title": "Drafter",
       "description": "Join us.",
-      "location": "Jacksonville",
+      "location": "Jacksonville, FL",
+      "locationSlug": "jacksonville-fl",
       "department": "Drafting"
     }
   ]
